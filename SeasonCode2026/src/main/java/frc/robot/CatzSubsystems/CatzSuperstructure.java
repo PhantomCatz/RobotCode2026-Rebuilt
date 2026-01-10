206c30e8b22f706e33a97d31f23cd0191ba475a1
YKS44 <79933420+YKS44@users.noreply.github.com>
2026-01-10 13:10:31 -0800

    add shooting regression

--- SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzSuperstructure.java
+++ SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzSuperstructure.java
@@ -1,5 +1,28 @@
 package frc.robot.CatzSubsystems;
 
+import frc.robot.Utilities.InterpolatingDouble;
+import frc.robot.regressions.ShooterRegression;
+
 public class CatzSuperstructure {
-    
+    public static final CatzSuperstructure Instance = new CatzSuperstructure();
+
+    private CatzSuperstructure(){}
+
+    // interpolates distance to target for shooter setpoint along regression
+    private double getShooterSetpointFromRegression(double range) {
+        if (ShooterRegression.kUseFlywheelAutoAimPolynomial) {
+            return ShooterRegression.kFlywheelAutoAimPolynomial.predict(range);
+        } else {
+            return ShooterRegression.kFlywheelAutoAimMap.getInterpolated(new InterpolatingDouble(range)).value;
+        }
+    }
+
+    // interpolates distance to target for hood setpoint along regression
+    private double getHoodSetpointFromRegression(double range) {
+        if (ShooterRegression.kUseHoodAutoAimPolynomial) {
+            return ShooterRegression.kHoodAutoAimPolynomial.predict(range);
+        } else {
+            return ShooterRegression.kHoodAutoAimMap.getInterpolated(new InterpolatingDouble(range)).value;
+        }
+    }
 }
---
+++ SeasonCode2026/src/main/java/frc/robot/regressions/ShooterRegression.java
@@ -0,0 +1,58 @@
+package frc.robot.regressions;
+
+import frc.robot.Utilities.InterpolatingDouble;
+import frc.robot.Utilities.InterpolatingTreeMap;
+import frc.robot.Utilities.PolynomialRegression;
+
+public class ShooterRegression {
+    public static final double kHoodPaddingDegrees = 2;
+    public static final double kShooterPaddingVelocity = 100;
+
+
+    public static final double[] kPadding = {
+            kShooterPaddingVelocity, kHoodPaddingDegrees};
+
+    //hood
+    public static double kDefaultHoodAngle = Math.toRadians(0);
+    public static boolean kUseHoodAutoAimPolynomial = false;
+
+    public static boolean kUseSmartdashboard = false;
+
+    public static InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> kHoodAutoAimMap = new InterpolatingTreeMap<>();
+    public static PolynomialRegression kHoodAutoAimPolynomial;
+
+    public static double[][] kHoodRegression;
+
+    static {
+
+        kHoodRegression = EpsilonRegression.kHoodManualAngle;
+        
+        //iterate through the array and place each point into the interpolating tree
+        for (double[] pair : kHoodRegression) {
+            kHoodAutoAimMap.put(new InterpolatingDouble(pair[0]), new InterpolatingDouble(pair[1]));
+        }
+        
+        kHoodAutoAimPolynomial = new PolynomialRegression(kHoodRegression, 1);
+    }
+    
+    //shooter
+    public static double kDefaultShootingRPM = 2950.0;
+    public static boolean kUseFlywheelAutoAimPolynomial = false;
+
+    public static InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> kFlywheelAutoAimMap = new InterpolatingTreeMap<>();
+    public static PolynomialRegression kFlywheelAutoAimPolynomial;
+
+    public static double[][] kFlywheelRegression;
+
+    static {
+
+        kFlywheelRegression = EpsilonRegression.kFlywheelManualRPM;
+        
+        for (double[] pair : kFlywheelRegression) {
+            kFlywheelAutoAimMap.put(new InterpolatingDouble(pair[0]), new InterpolatingDouble(pair[1]));
+        }
+
+        kFlywheelAutoAimPolynomial = new PolynomialRegression(kFlywheelRegression, 2);
+    }
+
+}
