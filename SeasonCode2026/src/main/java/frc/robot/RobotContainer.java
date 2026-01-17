9e9d321845f1bf1ca92d7005c0e81cbaa845388c
Evan13019 <echwang3@gmail.com>
2026-01-17 11:06:00 -0800

    remove unecesary

--- SeasonCode2026/src/main/java/frc/robot/RobotContainer.java
+++ SeasonCode2026/src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,16 @@
 package frc.robot;
 
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.CatzSubsystems.CatzSuperstructure;
+import frc.robot.CatzSubsystems.CatzClimb.CatzClimb;
+import frc.robot.CatzSubsystems.CatzDriveAndRobotOrientation.Drivetrain.CatzDrivetrain;
+import frc.robot.CatzSubsystems.CatzHood.CatzHood;
 import frc.robot.CatzSubsystems.CatzIntake.CatzIntake;
 import frc.robot.CatzSubsystems.CatzIntake.IntakeConstants;
-import frc.robot.CatzSubsystems.CatzShooter.CatzFlywheels;
-import frc.robot.Utilities.Setpoint;
+import frc.robot.CatzSubsystems.CatzTurret.CatzTurret;
+import frc.robot.Commands.DriveAndRobotOrientationCmds.TeleopDriveCmd;
 
 public class RobotContainer {
   private final CatzSuperstructure superstructure = CatzSuperstructure.Instance;
@@ -16,25 +18,17 @@ public class RobotContainer {
   private final CommandXboxController xboxDrv = new CommandXboxController(0);
 
   public RobotContainer() {
-    System.out.println("Drivetrain Initializing" + CatzDrivetrain.Instance.getName());
-    System.out.println("Turret Initializing" + CatzTurret.Instance.getName());
-    System.out.println("Hood Initializing" + CatzHood.Instance.getName());
-    System.out.println("Intake Initializing" + CatzIntake.Instance.getName());
-    System.out.println("Initializing Climb" + CatzClimb.Instance.getName());
 
     configureBindings();
   }
 
   private void configureBindings() {
-    // CatzDrivetrain.Instance.setDefaultCommand(new TeleopDriveCmd(() -> xboxDrv.getLeftX(), () -> xboxDrv.getLeftY(), () -> xboxDrv.getRightX(), CatzDrivetrain.Instance));
+    CatzDrivetrain.Instance.setDefaultCommand(new TeleopDriveCmd(() -> xboxDrv.getLeftX(), () -> xboxDrv.getLeftY(), () -> xboxDrv.getRightX(), CatzDrivetrain.Instance));
 
     xboxDrv.a().onTrue(superstructure.turretTrackCommand());
     xboxDrv.x().onTrue(superstructure.turretStowCommand().alongWith(superstructure.hoodFlywheelStowCommand()));
 
-    xboxDrv.leftBumper().onTrue(superstructure.shootingTuneCommand());
-
-
-    xboxDrv.b().onTrue(CatzFlywheels.Instance.setpointCommand(Setpoint.withVelocitySetpointVoltage(100.0)));
+    xboxDrv.b().onTrue(superstructure.applyShooterSetpoint());
   }
 
   public Command getAutonomousCommand() {
