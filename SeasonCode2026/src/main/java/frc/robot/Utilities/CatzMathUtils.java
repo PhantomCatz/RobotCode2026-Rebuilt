ff3dcc646ac173b7195e067796afc4009978a376
Kynam L <115427320+Noto501@users.noreply.github.com>
2026-01-18 12:09:23 -0800

    Update CatzMathUtils.java

--- SeasonCode2026/src/main/java/frc/robot/Utilities/CatzMathUtils.java
+++ SeasonCode2026/src/main/java/frc/robot/Utilities/CatzMathUtils.java
@@ -9,7 +9,7 @@ public class CatzMathUtils {
   public static SwerveModuleState optimize(
       SwerveModuleState desiredState, Rotation2d currentAngle) {
     double targetAngle =
-        placeInAppropriate0To360Scope(currentAngle.getDegrees(), desiredState.angle.getDegrees());
+        placeTargetInAppropriate0To360Scope(currentAngle.getDegrees(), desiredState.angle.getDegrees());
     double targetSpeed = desiredState.speedMetersPerSecond;
     double delta = targetAngle - currentAngle.getDegrees();
     if (Math.abs(delta) > 90) {
@@ -24,7 +24,7 @@ public class CatzMathUtils {
    * @param newAngle Target Angle
    * @return Closest angle within scope
    */
-  public static double placeInAppropriate0To360Scope(double scopeReference, double newAngle) {
+  public static double placeTargetInAppropriate0To360Scope(double scopeReference, double newAngle) {
     double lowerBound;
     double upperBound;
     double lowerOffset = scopeReference % 360;
@@ -76,31 +76,29 @@ public class CatzMathUtils {
    */
   public static double getOptimizedAngleWithLimits(double currentAngle, double targetAngle, double minLimit, double maxLimit) {
       
-      // find shortest path to target angle based off current angle
-      double directTarget = placeInAppropriate0To360Scope(currentAngle, targetAngle);
+      //find shortest path to target angle based off current angle
+      double directTarget = placeTargetInAppropriate0To360Scope(currentAngle, targetAngle);
 
-      // check if the shortest path is valid
+      //check if the shortest path is valid
       if (directTarget <= maxLimit && directTarget >= minLimit) {
           return directTarget;
       }
 
-      // if blocked, shift by 360
+      //if blocked, shift by 360 depending on which limit we exceeded
       double altTarget;
       if (directTarget > maxLimit) {
-          // hit the top limit, subtracting 360 to go under
-          altTarget = directTarget - 360;
+          altTarget = directTarget - 360.0;
       } else {
-          // hit bottom limit,  adding 360 to go over
-          altTarget = directTarget + 360;
+          altTarget = directTarget + 360.0;
       }
 
-      // check if the alt path is valid
+      //check if the alt path is valid
       if (altTarget <= maxLimit && altTarget >= minLimit) {
           return altTarget;
       }
 
       // emergency clamp
-      // we return the limit closest to the desired target to stay safe
+      //Return the limit closest to the desired target to stay safe
       if (directTarget > maxLimit) {
           return maxLimit;
       } else {
