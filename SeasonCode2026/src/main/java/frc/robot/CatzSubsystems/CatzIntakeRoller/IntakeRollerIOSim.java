f0967078450f978ad2e212839b7f27a1a19cf8b2
YKS44 <79933420+YKS44@users.noreply.github.com>
2026-01-23 10:13:04 -0800

    made proper sim constructor for intake roller

--- SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzIntakeRoller/IntakeRollerIOSim.java
+++ SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzIntakeRoller/IntakeRollerIOSim.java
@@ -1,7 +1,10 @@
 package frc.robot.CatzSubsystems.CatzIntakeRoller;
 
 import frc.robot.CatzAbstractions.io.GenericIOSim;
+import frc.robot.Utilities.MotorUtil.Gains;
 
 public class IntakeRollerIOSim extends GenericIOSim<IntakeRollerIO.IntakeRollerIOInputs> implements IntakeRollerIO{
-
+    public IntakeRollerIOSim(Gains gains){
+        super(gains);
+    }    
 }
