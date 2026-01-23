8bc2c969609742f32902711218afffb294c7d8e7
YKS44 <79933420+YKS44@users.noreply.github.com>
2026-01-23 10:06:49 -0800

    change roller autologged name properly

--- SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzIntakeRoller/CatzIntakeRoller.java
+++ SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzIntakeRoller/CatzIntakeRoller.java
@@ -4,12 +4,11 @@ import org.littletonrobotics.junction.Logger;
 
 import frc.robot.CatzConstants;
 import frc.robot.CatzAbstractions.Bases.GenericMotorSubsystem;
-import frc.robot.CatzSubsystems.CatzIntake.IntakeIOInputsAutoLogged;
 
 public class CatzIntakeRoller extends GenericMotorSubsystem<IntakeRollerIO, IntakeRollerIO.IntakeRollerIOInputs>{
 
     private static final IntakeRollerIO io = getIOInstance();
-    private static final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
+    private static final IntakeRollerIOInputsAutoLogged inputs = new IntakeRollerIOInputsAutoLogged();
 
     public static final CatzIntakeRoller Instance = new CatzIntakeRoller();
 
@@ -20,7 +19,7 @@ public class CatzIntakeRoller extends GenericMotorSubsystem<IntakeRollerIO, Inta
     }
 
     private CatzIntakeRoller() {
-        super(io, inputs, "CatzIntake");
+        super(io, inputs, "CatzIntakeRoller");
     }
 
     private static IntakeRollerIO getIOInstance() {
--- SeasonCode2026/src/main/java/frc/robot/RobotContainer.java
+++ SeasonCode2026/src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,11 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.CatzSubsystems.CatzSuperstructure;
 import frc.robot.CatzSubsystems.CatzDriveAndRobotOrientation.CatzRobotTracker;
 import frc.robot.CatzSubsystems.CatzDriveAndRobotOrientation.Drivetrain.CatzDrivetrain;
-import frc.robot.CatzSubsystems.CatzIntakeRoller.CatzIntakeRoller;
-import frc.robot.CatzSubsystems.CatzIntakeRoller.IntakeRollerConstants;
 import frc.robot.CatzSubsystems.CatzTurret.CatzTurret;
 import frc.robot.Commands.DriveAndRobotOrientationCmds.TeleopDriveCmd;
 
