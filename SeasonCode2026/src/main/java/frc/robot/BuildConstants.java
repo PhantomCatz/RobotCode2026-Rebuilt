196d3f9646af70ded006400540c68ecf24379fbe
Soap_will_i_am <w1wangwilliam@gmail.com>
2026-01-24 15:53:10 -0800

    added multithread from 2025 code

--- SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 99;
-  public static final String GIT_SHA = "e740b751f26b0dad7994549fc8658c48e2364a75";
-  public static final String GIT_DATE = "2026-01-24 12:08:19 EST";
+  public static final int GIT_REVISION = 100;
+  public static final String GIT_SHA = "c97b389e39b92f48e45dbdb529e5999a6f8074cb";
+  public static final String GIT_DATE = "2026-01-24 18:11:25 EST";
   public static final String GIT_BRANCH = "turret-tracking-tune";
-  public static final String BUILD_DATE = "2026-01-24 17:42:46 EST";
-  public static final long BUILD_UNIX_TIME = 1769294566522L;
+  public static final String BUILD_DATE = "2026-01-24 18:52:20 EST";
+  public static final long BUILD_UNIX_TIME = 1769298740689L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
