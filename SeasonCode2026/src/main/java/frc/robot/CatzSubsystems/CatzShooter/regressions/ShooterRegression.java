4bd054a46e4f0ef0751ff27e1fc36ac63e6488df
YKS44 <79933420+YKS44@users.noreply.github.com>
2026-01-26 11:44:11 -0800

    add airtime regression

--- SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzShooter/regressions/ShooterRegression.java
+++ SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzShooter/regressions/ShooterRegression.java
@@ -1,7 +1,10 @@
 package frc.robot.CatzSubsystems.CatzShooter.regressions;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Distance;
+import frc.robot.FieldConstants;
+import frc.robot.CatzSubsystems.CatzDriveAndRobotOrientation.CatzRobotTracker;
 import frc.robot.Utilities.InterpolatingDouble;
 import frc.robot.Utilities.InterpolatingTreeMap;
 import frc.robot.Utilities.PolynomialRegression;
@@ -13,33 +16,41 @@ public class ShooterRegression {
     public static final double[] kPadding = {
             kShooterPaddingVelocity, kHoodPaddingDegrees};
 
-    //hood
-    public static double kDefaultHoodAngle = Math.toRadians(0);
-    public static boolean kUseHoodAutoAimPolynomial = false;
 
     public static boolean kUseSmartdashboard = false;
-
-    public static InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> kHoodAutoAimMap = new InterpolatingTreeMap<>();
-    public static PolynomialRegression kHoodAutoAimPolynomial;
 
     //shooter
     public static double kDefaultShootingRPM = 2950.0;
     public static boolean kUseFlywheelAutoAimPolynomial = false;
 
-    public static InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> kFlywheelAutoAimMap = new InterpolatingTreeMap<>();
-    public static PolynomialRegression kFlywheelAutoAimPolynomial;
+    public static InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> flywheelAutoAimMap = new InterpolatingTreeMap<>();
+    public static PolynomialRegression flywheelAutoAimPolynomial;
 
-    public static double[][] kFlywheelRegression;
+    public static InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> airtimeAutoAimMap = new InterpolatingTreeMap<>();
+    public static PolynomialRegression airtimeAutoAimPolynomial;
+
+    public static double[][] flywheelRegression;
+    public static double[][] airtimeRegression;
 
     static {
 
-        kFlywheelRegression = EpsilonRegression.kFlywheelManualRPM;
+        flywheelRegression = EpsilonRegression.flywheelManualRPM;
 
-        for (double[] pair : kFlywheelRegression) {
-            kFlywheelAutoAimMap.put(new InterpolatingDouble(pair[0]), new InterpolatingDouble(pair[1]));
+        for (double[] pair : flywheelRegression) {
+            flywheelAutoAimMap.put(new InterpolatingDouble(pair[0]), new InterpolatingDouble(pair[1]));
         }
 
-        kFlywheelAutoAimPolynomial = new PolynomialRegression(kFlywheelRegression, 2);
+        flywheelAutoAimPolynomial = new PolynomialRegression(flywheelRegression, 2);
+    }
+
+    static {
+        airtimeRegression = EpsilonRegression.airtime;
+
+        for(double[] pair : airtimeRegression){
+            airtimeAutoAimMap.put(new InterpolatingDouble(pair[0]), new InterpolatingDouble(pair[1]));
+        }
+
+        airtimeAutoAimPolynomial = new PolynomialRegression(airtimeRegression, 2);
     }
 
     private static final double HOOD_ANGLE_SLOPE = (EpsilonRegression.FARTHEST_HOOD_ANGLE[1]-EpsilonRegression.CLOSEST_HOOD_ANGLE[1]) /
@@ -47,6 +58,12 @@ public class ShooterRegression {
 
     public static double getHoodAngle(Distance distance){
         return HOOD_ANGLE_SLOPE * (distance.in(Units.Meters) - EpsilonRegression.CLOSEST_HOOD_ANGLE[0]) + EpsilonRegression.CLOSEST_HOOD_ANGLE[1];
+    }
+
+    public static double getFutureDistance(Translation2d robotSpeed){
+        Translation2d robotPose = CatzRobotTracker.Instance.getEstimatedPose().getTranslation();
+        
+        Translation2d displacementVector = 
     }
 
 }
