3be3e09b5cd60b7da51db350394c3792249fed69
YKS44 <79933420+YKS44@users.noreply.github.com>
2026-01-29 09:18:30 -0800

    declutter superstructure

--- SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzSuperstructure.java
+++ SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzSuperstructure.java
@@ -16,10 +16,6 @@ import frc.robot.FieldConstants;
 import frc.robot.CatzSubsystems.CatzDriveAndRobotOrientation.CatzRobotTracker;
 import frc.robot.CatzSubsystems.CatzHood.CatzHood;
 import frc.robot.CatzSubsystems.CatzHood.HoodConstants;
-import frc.robot.CatzSubsystems.CatzIntakeDeploy.CatzIntakeDeploy;
-import frc.robot.CatzSubsystems.CatzIntakeDeploy.IntakeDeployConstants;
-import frc.robot.CatzSubsystems.CatzIntakeRoller.CatzIntakeRoller;
-import frc.robot.CatzSubsystems.CatzIntakeRoller.IntakeRollerConstants;
 import frc.robot.CatzSubsystems.CatzShooter.CatzFlywheels;
 import frc.robot.CatzSubsystems.CatzShooter.FlywheelConstants;
 import frc.robot.CatzSubsystems.CatzShooter.regressions.ShooterRegression;
@@ -46,11 +42,6 @@ public class CatzSuperstructure {
         return CatzTurret.Instance.followSetpointCommand(() -> calculateHubTrackingSetpointNoOffset());
 
     }
-
-    public Command turretStowCommand() {
-        return CatzTurret.Instance.setpointCommand(TurretConstants.HOME_SETPOINT);
-    }
-
     public Command turretManualTrackCommand() {
         // return CatzTurret.Instance.setpointCommand(Setpoint.withDutyCycleSetpoint(0.1));
         // return CatzTurret.Instance.setpointCommand(Setpoint.withPositionSetpoint(Units.Degrees.of(90.0)));
@@ -121,26 +112,6 @@ public class CatzSuperstructure {
         }, Set.of(CatzFlywheels.Instance));
     }
 
-    public Command hoodTestCommand(){
-        return CatzHood.Instance.setpointCommand(HoodConstants.HOOD_TEST_SETPOINT);
-    }
-
-    public Command applyHoodSetpoint(){
-        return CatzHood.Instance.setpointCommand(HoodConstants.HOOD_TEST_SETPOINT);
-    }
-
-    public Command IntakeOn(){
-        return CatzIntakeRoller.Instance.setpointCommand(IntakeRollerConstants.H_SETPOINT);
-    }
-
-    public Command IntakeOff(){
-        return CatzIntakeRoller.Instance.setpointCommand(IntakeRollerConstants.OFF_SETPOINT);
-    }
-
-    public Command SlapDown() {
-        return CatzIntakeDeploy.Instance.setpointCommand(IntakeDeployConstants.HoldDown);
-    }
-
     /**
      * Calculates the best turret angle setpoint to point to the hub
      * while respecting physical limits and minimizing movement
@@ -198,10 +169,5 @@ public class CatzSuperstructure {
             return ShooterRegression.kHoodAutoAimMap.getInterpolated(new InterpolatingDouble(range)).value;
         }
     }
-
-    // public Command shootTuning(){
-    // return
-    // CatzFlywheels.Instance.setpointCommand(CatzShooter.Instance.getTunableSetpoint());
-    // }
 
 }
