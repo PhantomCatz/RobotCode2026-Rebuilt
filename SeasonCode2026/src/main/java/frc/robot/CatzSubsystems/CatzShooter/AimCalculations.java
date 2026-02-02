7ab3351a3f29952db3876cd6851f18e2daec299c
William <149019293+soap-will-i-am@users.noreply.github.com>
2026-02-02 14:12:36 -0800

    oopsies

--- SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzShooter/AimCalculations.java
+++ SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzShooter/AimCalculations.java
@@ -38,8 +38,7 @@ public class AimCalculations {
 
         Translation2d hubDirection = FieldConstants.HUB_LOCATION.minus(CatzTurret.Instance.getFieldToTurret());
 
-        double targetRads = hubDirection.getAngle().getRadians()
-                - fieldToRobot.getRotation().minus(TurretConstants.TURRET_ROTATION_OFFSET).getRadians();
+        double targetRads = hubDirection.getAngle().minus(fieldToRobot.getRotation()).minus(TurretConstants.TURRET_ROTATION_OFFSET).getRadians();
         // if(DriverStation.getAlliance().get() == Alliance.Red){
         // targetRads -= Math.PI;
         // }
