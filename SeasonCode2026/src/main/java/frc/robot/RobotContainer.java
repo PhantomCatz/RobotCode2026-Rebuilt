42588361e5f0282349b7c44d8bf426d97b329f64
YKS44 <79933420+YKS44@users.noreply.github.com>
2026-02-05 12:22:53 -0800

    add proper gyro reset

--- SeasonCode2026/src/main/java/frc/robot/RobotContainer.java
+++ SeasonCode2026/src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,8 @@ import frc.robot.CatzSubsystems.CatzShooter.CatzTurret.CatzTurret;
 import frc.robot.CatzSubsystems.CatzShooter.regressions.ShooterRegression;
 import frc.robot.CatzSubsystems.CatzVision.ApriltagScanning.LimelightSubsystem;
 import frc.robot.Commands.DriveAndRobotOrientationCmds.TeleopDriveCmd;
+import frc.robot.Utilities.AllianceFlipUtil;
+import frc.robot.Utilities.DoublePressTracker;
 
 public class RobotContainer {
   private final CatzSuperstructure superstructure = CatzSuperstructure.Instance;
@@ -22,7 +24,7 @@ public class RobotContainer {
   private static final CommandXboxController xboxDrv = new CommandXboxController(0);
   private static final CommandXboxController xboxTest = new CommandXboxController(1);
 
-  public RobotContainer(){
+  public RobotContainer() {
     configureBindings();
 
     var turret = CatzTurret.Instance;
@@ -30,23 +32,34 @@ public class RobotContainer {
     var vision = LimelightSubsystem.Instance;
     var regression = ShooterRegression.TUNABLE_HOOD_ANGLE_MIN;
   }
+
   private void configureBindings() {
-    CatzDrivetrain.Instance.setDefaultCommand(new TeleopDriveCmd(() -> xboxDrv.getLeftX(), () -> xboxDrv.getLeftY(), () -> xboxDrv.getRightX(), CatzDrivetrain.Instance));
+    CatzDrivetrain.Instance.setDefaultCommand(new TeleopDriveCmd(() -> xboxDrv.getLeftX(), () -> xboxDrv.getLeftY(),
+        () -> xboxDrv.getRightX(), CatzDrivetrain.Instance));
     // CatzTurret.Instance.setDefaultCommand(
-    //   superstructure.turretManualTrackCommand()
-    xboxDrv.start().onTrue(new InstantCommand(() -> CatzRobotTracker.Instance.resetPose(new Pose2d(new Translation2d(), new Rotation2d()))));
+    // superstructure.turretManualTrackCommand()
+    DoublePressTracker.createTrigger(xboxDrv.back()).onTrue(new InstantCommand(() -> {
+      if (AllianceFlipUtil.shouldFlip()) {
+        CatzRobotTracker.Instance
+            .resetPose(new Pose2d(CatzRobotTracker.Instance.getEstimatedPose().getTranslation(), Rotation2d.k180deg));
+      } else {
+        CatzRobotTracker.Instance
+            .resetPose(new Pose2d(CatzRobotTracker.Instance.getEstimatedPose().getTranslation(), new Rotation2d()));
+      }
+    }));
     // );
 
-    //----------------------Shooting-----------------------
+    // ----------------------Shooting-----------------------
     xboxDrv.leftBumper().onTrue(superstructure.prepareForShooting());
     xboxDrv.leftBumper().onFalse(superstructure.setShootingAllowed(true));
     xboxDrv.x().onTrue(superstructure.stopAllShooting());
 
-    //---------------------Testing Controls--------------------
-    xboxTest.b().onTrue(superstructure.applyFlywheelTuningSetpoint().alongWith(superstructure.applyHoodTuningSetpoint()).alongWith(superstructure.turretTrackHubCommand()));
+    // ---------------------Testing Controls--------------------
+    xboxTest.b().onTrue(superstructure.applyFlywheelTuningSetpoint().alongWith(superstructure.applyHoodTuningSetpoint())
+        .alongWith(superstructure.turretTrackHubCommand()));
     // xboxTest.b().onTrue(superstructure.applyFlywheelTuningSetpoint());
     // xboxTest.b().onTrue(superstructure.interpolateHoodAngle()
-    //                     .alongWith(superstructure.interpolateShooterSpeed()).alongWith(superstructure.turretTrackCommand()));
+    // .alongWith(superstructure.interpolateShooterSpeed()).alongWith(superstructure.turretTrackCommand()));
     xboxTest.leftBumper().onTrue(superstructure.turretTrackHubCommand());
 
     // xboxTest.leftBumper().onTrue(superstructure.turret90Degrees());
@@ -56,7 +69,7 @@ public class RobotContainer {
     xboxTest.x().onTrue(superstructure.stopAllShooting());
   }
 
-  public static void rumbleDrv(double val){
+  public static void rumbleDrv(double val) {
     xboxDrv.setRumble(RumbleType.kBothRumble, val);
   }
 }
