a7f2cba0a0b415c1743187b680c8fc213c5007de
William <149019293+soap-will-i-am@users.noreply.github.com>
2026-02-05 13:18:34 -0800

    2/5 lunch changes

--- SeasonCode2026/src/main/java/frc/robot/Autonomous/routines/DepotClimb.java
+++ SeasonCode2026/src/main/java/frc/robot/Autonomous/routines/DepotClimb.java
@@ -3,48 +3,40 @@ package frc.robot.Autonomous.routines;
 import choreo.auto.AutoTrajectory;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Autonomous.AutoRoutineBase;
 import frc.robot.Autonomous.AutonConstants;
 
 public class DepotClimb extends AutoRoutineBase{
     public DepotClimb(){
         super("DepotClimb");
-        AutoTrajectory trajpre = getTrajectory("DepotClimb");
-        // AutoTrajectory traj1 = getTrajectory("DepotClimb",1);
-        // AutoTrajectory traj2 = getTrajectory("DepotClimb",2);
-        // AutoTrajectory traj3 = getTrajectory("DepotClimb",3);
-        // AutoTrajectory traj4 = getTrajectory("DepotClimb",4);
-        // AutoTrajectory traj5 = getTrajectory("DepotClimb",5);
+        AutoTrajectory traj1 = getTrajectory("DepotClimb",0);
+        AutoTrajectory traj2 = getTrajectory("DepotClimb",1);
+        AutoTrajectory traj3 = getTrajectory("DepotClimb",2);
+        AutoTrajectory traj4 = getTrajectory("DepotClimb",3);
+        AutoTrajectory traj5 = getTrajectory("DepotClimb",4);
 
         prepRoutine(
-            trajpre,
-            new ParallelCommandGroup(
-                followTrajectoryWithAccuracy(trajpre),
-                // Commands.waitUntil(traj1.atTime("Score1")).andThen(CatzSuperstructure.Instance.Shoot()),
-                // Commands.waitUntil(traj1.atTime("Intake2")).andThen(CatzSuperstructure.Instance.Intake()),
-                // Commands.waitUntil(traj1.atTime("Score3")).andThen(CatzSuperstructure.Instance.Shoot()),
-                // Commands.waitUntil(traj1.atTime("Climb5")).andThen(CatzSuperstructure.Instance.Climb())
-                Commands.waitUntil(trajpre.atPose("Score1", AutonConstants.EM_ACCEPTABLE_DIST_METERS, AutonConstants.EM_ACCEPTABLE_RADS))
-                        .andThen(() -> System.out.println("ONE \n")),
-                Commands.waitUntil(trajpre.atPose("Intake2", AutonConstants.EM_ACCEPTABLE_DIST_METERS, AutonConstants.EM_ACCEPTABLE_RADS))
-                        .andThen(Commands.print("TWO \n")),
-                Commands.waitUntil((trajpre.atTime("Score3")))
-                        .andThen(Commands.print("THREE \n")),
-                Commands.waitUntil(trajpre.atPose("Climb5", AutonConstants.EM_ACCEPTABLE_DIST_METERS, AutonConstants.EM_ACCEPTABLE_RADS))
-                        .andThen(Commands.print("FIVE \n"))
+            traj1,
+            // new ParallelCommandGroup(
+            //     followTrajectoryWithAccuracy(traj1),
+            //     Commands.waitUntil(traj1.atPose("Score1", AutonConstants.EM_ACCEPTABLE_DIST_METERS, AutonConstants.EM_ACCEPTABLE_RADS))
+            //             .andThen(() -> System.out.println("ONE \n"))
+            //     // Commands.waitUntil(trajpre.atPose("Intake2", AutonConstants.EM_ACCEPTABLE_DIST_METERS, AutonConstants.EM_ACCEPTABLE_RADS))
+            //     //         .andThen(Commands.print("TWO \n")),
+            //     // Commands.waitUntil((trajpre.atTime("Score3")))
+            //     //         .andThen(Commands.print("THREE \n")),
+            //     // Commands.waitUntil(trajpre.atPose("Climb5", AutonConstants.EM_ACCEPTABLE_DIST_METERS, AutonConstants.EM_ACCEPTABLE_RADS))
+            //     //         .andThen(Commands.print("FIVE \n"))
 
+            // ),
+            new SequentialCommandGroup(
+                new ParallelCommandGroup(
+                    followTrajectoryWithAccuracy(traj1),
+                    Commands.waitUntil(traj1.atPose("Score1", AutonConstants.EM_ACCEPTABLE_DIST_METERS, AutonConstants.EM_ACCEPTABLE_RADS))
+                            .andThen(() -> System.out.println("ONE \n"))
+                )
             )
-
-            // // Commands.waitUntil(traj1.atTime("Score")).andThen(() -> CatzSuperstructure.Instance.applyShooterSetpoint()), // CatzSuperstructure.Instance.ScoreFuel(),
-            // followTrajectoryWithAccuracy(traj1),
-            // followTrajectoryWithAccuracy(traj2),
-            // // Commands.waitUntil(traj1.atTime("Intake")).andThen(() -> CatzSuperstructure.Instance.Intake()),              // CatzSuperstructure.Instance.IntakeFuel(),
-            // followTrajectoryWithAccuracy(traj3),
-            // // CatzSuperstructure.Instance.ScoreFuel(),
-            // followTrajectoryWithAccuracy(traj4),
-            // followTrajectoryWithAccuracy(traj5)
-            // // CatzSuperstructure.Instance.Climb(),
-
         );
     }
 }
--- SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 65;
-  public static final String GIT_SHA = "17545191f5c9810257f165830a8b93f3164b867b";
-  public static final String GIT_DATE = "2026-02-02 16:48:41 EST";
+  public static final int GIT_REVISION = 66;
+  public static final String GIT_SHA = "801809fcbb89bcb174f383795bd0b3014298bdc3";
+  public static final String GIT_DATE = "2026-02-04 20:56:35 EST";
   public static final String GIT_BRANCH = "add-auton-paths";
-  public static final String BUILD_DATE = "2026-02-04 20:47:58 EST";
-  public static final long BUILD_UNIX_TIME = 1770256078191L;
+  public static final String BUILD_DATE = "2026-02-05 16:17:39 EST";
+  public static final long BUILD_UNIX_TIME = 1770326259413L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
