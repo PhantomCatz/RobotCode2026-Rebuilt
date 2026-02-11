a0d452ac5911274d0e82a61f4e4c8faf99215957
William W <149019293+soap-will-i-am@users.noreply.github.com>
2026-02-11 13:28:51 -0800

    Jaden path changes
    bro had to go early so he'll do the rest later

--- SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 192;
-  public static final String GIT_SHA = "6130dc88460600fc2a00e41f65aa10aa596436f6";
-  public static final String GIT_DATE = "2026-02-08 17:38:37 EST";
+  public static final int GIT_REVISION = 193;
+  public static final String GIT_SHA = "500586ab692be48fa0f3544a1617e79ab1b53ce5";
+  public static final String GIT_DATE = "2026-02-08 20:49:38 EST";
   public static final String GIT_BRANCH = "add-auton-paths";
-  public static final String BUILD_DATE = "2026-02-08 20:05:05 EST";
-  public static final long BUILD_UNIX_TIME = 1770599105060L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-09 16:04:08 EST";
+  public static final long BUILD_UNIX_TIME = 1770671048521L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
