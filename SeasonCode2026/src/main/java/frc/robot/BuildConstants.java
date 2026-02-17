4a388bd39a4b5e584e63918d5c5174ca8711aab3
YKS44 <79933420+YKS44@users.noreply.github.com>
2026-02-16 20:08:40 -0800

    fix build error

--- SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 248;
-  public static final String GIT_SHA = "21feaf314746a75f1e0766aa868c8368271c3737";
-  public static final String GIT_DATE = "2026-02-15 19:03:33 EST";
+  public static final int GIT_REVISION = 253;
+  public static final String GIT_SHA = "6bf1b3b42d268d4bcb8a37d7135a87a87f844d98";
+  public static final String GIT_DATE = "2026-02-16 23:05:09 EST";
   public static final String GIT_BRANCH = "irl-auton-test";
-  public static final String BUILD_DATE = "2026-02-15 20:27:08 EST";
-  public static final long BUILD_UNIX_TIME = 1771205228888L;
+  public static final String BUILD_DATE = "2026-02-16 23:08:07 EST";
+  public static final long BUILD_UNIX_TIME = 1771301287055L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
