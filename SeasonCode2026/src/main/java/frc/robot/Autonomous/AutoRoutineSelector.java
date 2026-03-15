ed29c66b7b6e1cf222acc97a921e6ea713d68787
William W <149019293+soap-will-i-am@users.noreply.github.com>
2026-03-15 15:32:48 -0700

    Added 2 paths w/ code
    Depot_1_Cycle_Disrupt and Depot_2_Cycle_Bump

--- SeasonCode2026/src/main/java/frc/robot/Autonomous/AutoRoutineSelector.java
+++ SeasonCode2026/src/main/java/frc/robot/Autonomous/AutoRoutineSelector.java
@@ -11,26 +11,31 @@ public class AutoRoutineSelector {
     private AutoChooser autoSelector = new AutoChooser();
 
     private AutoRoutineSelector(){
-        autoSelector.addRoutine("Test", () -> new Test().getRoutine());
-
+        
         autoSelector.addRoutine("Decon_Depot_1_Cycle", () -> new Decon_Depot_1_Cycle().getRoutine());
         autoSelector.addRoutine("Decon_Depot_2_Cycle", () -> new Decon_Depot_2_Cycle().getRoutine());
         // autoSelector.addRoutine("Decon_Outpost_2_Cycle_Climb", () -> new Decon_Outpost_2_Cycle_Climb().getRoutine());
         autoSelector.addRoutine("Decon_Outpost_2_Cycle_Outpost", () -> new Decon_Outpost_2_Cycle_Outpost().getRoutine());
         // autoSelector.addRoutine("Decon_Outpost_Climb", () -> new Decon_Outpost_Climb().getRoutine());
         autoSelector.addRoutine("Decon_PNZO", () -> new Decon_PNZO().getRoutine());
+        autoSelector.addRoutine("Depot_1_Cycle_Disrupt", () -> new Depot_1_Cycle_Disrupt().getRoutine());
         autoSelector.addRoutine("Depot_1_Cycle", () -> new Depot_1_Cycle().getRoutine());
+        autoSelector.addRoutine("Depot_2_Cycle_Bump", () -> new Depot_2_Cycle_Bump().getRoutine());
+        autoSelector.addRoutine("Depot_2_Cycle_Bump_Fast_Decon", () -> new Depot_2_Cycle_Bump_Fast_Decon().getRoutine());
         autoSelector.addRoutine("Depot_2_Cycle", () -> new Depot_2_Cycle().getRoutine());
         autoSelector.addRoutine("Depot_3_Cycle", () -> new Depot_3_Cycle().getRoutine());
         autoSelector.addRoutine("Forefit_Depot", () -> new Forefit_Depot().getRoutine());
         autoSelector.addRoutine("Forefit_Outpost", () -> new Forefit_Outpost().getRoutine());
         autoSelector.addRoutine("MiddlePath", () -> new MiddlePath().getRoutine());
         autoSelector.addRoutine("Outpost_1_Cycle", () -> new Outpost_1_Cycle().getRoutine());
+        autoSelector.addRoutine("Outpost_2_Cycle_Bump", () -> new Outpost_2_Cycle_Bump().getRoutine());
         autoSelector.addRoutine("Outpost_2_Cycle_Outpost", () -> new Outpost_2_Cycle_Outpost().getRoutine());
         autoSelector.addRoutine("Outpost_2_Cycle", () -> new Outpost_2_Cycle().getRoutine());
         autoSelector.addRoutine("Outpost_3_Cycle", () -> new Outpost_3_Cycle().getRoutine());
+        autoSelector.addRoutine("Outpost_Hammerhead_2_Cycle_Outpost", () -> new Outpost_Hammerhead_2_Cycle_Outpost().getRoutine());
         autoSelector.addRoutine("PNZO", () -> new PNZO().getRoutine());
-
+        
+        autoSelector.addRoutine("Test", () -> new Test().getRoutine());
 
 
 
---
+++ SeasonCode2026/src/main/java/frc/robot/Autonomous/routines/Depot_1_Cycle_Disrupt.java
@@ -0,0 +1,49 @@
+package frc.robot.Autonomous.routines;
+
+import choreo.auto.AutoTrajectory;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Autonomous.AutoRoutineBase;
+import frc.robot.Autonomous.AutonConstants;
+import frc.robot.CatzSubsystems.CatzSuperstructure;
+
+public class Depot_1_Cycle_Disrupt extends AutoRoutineBase{
+    public Depot_1_Cycle_Disrupt(){
+        super("Depot_1_Cycle_Disrupt");
+        AutoTrajectory traj1 = getTrajectory("Depot_1_Cycle_Disrupt",0);
+        AutoTrajectory traj2 = getTrajectory("Depot_1_Cycle_Disrupt",1);
+        AutoTrajectory traj3 = getTrajectory("Depot_1_Cycle_Disrupt",2);
+        AutoTrajectory traj4 = getTrajectory("Depot_1_Cycle_Disrupt",3);
+
+
+        // traj2.atTime("Intake2").onTrue(CatzSuperstructure.Instance.intakeON());
+        // traj6.atTime("RampUp+StopIntake6").onTrue(CatzSuperstructure.Instance.intakeOFF());
+        // traj7.atTime("StowIntake+TrackTower7").onTrue();
+
+/*
+ * Note: this path is a troll path made by the strategy lead ;-;
+ */
+
+        prepRoutine(
+            traj1,
+            Commands.deadline(
+                Commands.sequence(
+                    CatzSuperstructure.Instance.deployIntake(),
+                    Commands.waitSeconds(AutonConstants.DEPLOY_INTAKE_WAIT),
+                    followTrajectory(traj1),
+                    CatzSuperstructure.Instance.intakeON(),
+                    followTrajectory(traj2),
+                    Commands.waitSeconds(1.5),
+                    followTrajectory(traj3),
+                    CatzSuperstructure.Instance.intakeOFF()
+                ),
+                CatzSuperstructure.Instance.trackStaticHub()
+            ),
+            Commands.deadline(
+                followTrajectoryWithAccuracy(traj4),
+                CatzSuperstructure.Instance.cmdHubStandby()
+            ),
+            shootAllBalls(13.0),
+            Commands.print("done")
+        );
+    }
+}
