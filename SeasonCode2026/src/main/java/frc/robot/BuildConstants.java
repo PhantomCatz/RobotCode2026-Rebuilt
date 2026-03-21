08e818c7de816e5560e0e68fec20230f2ff63f1a
William <149019293+soap-will-i-am@users.noreply.github.com>
2026-03-21 14:29:38 -0700

    WHY NO WORK

--- SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 453;
-  public static final String GIT_SHA = "330dc77d89cbacb8293d9fb9aec92c1558df3ce2";
-  public static final String GIT_DATE = "2026-03-20 22:30:20 EDT";
+  public static final int GIT_REVISION = 454;
+  public static final String GIT_SHA = "877a3e8be749f162e26729ded5367328437ffb09";
+  public static final String GIT_DATE = "2026-03-21 17:16:00 EDT";
   public static final String GIT_BRANCH = "0.1-second-delay";
-  public static final String BUILD_DATE = "2026-03-21 17:12:25 EDT";
-  public static final long BUILD_UNIX_TIME = 1774127545361L;
+  public static final String BUILD_DATE = "2026-03-21 17:27:16 EDT";
+  public static final long BUILD_UNIX_TIME = 1774128436970L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
