cbb4265b04fb63d8a5bcb31bcb89895219f7ad9b
Soap_will_i_am <w1wangwilliam@gmail.com>
2026-03-28 13:48:18 -0700

    default command
    nononono

--- SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 429;
-  public static final String GIT_SHA = "4fe38965933ea43cf05e9102a5f04a31596ccf09";
-  public static final String GIT_DATE = "2026-03-28 15:27:52 EDT";
+  public static final int GIT_REVISION = 430;
+  public static final String GIT_SHA = "7f688747b980943598b1bd755ceb0e858a9d444b";
+  public static final String GIT_DATE = "2026-03-28 16:44:53 EDT";
   public static final String GIT_BRANCH = "Toggle_Shooting";
-  public static final String BUILD_DATE = "2026-03-28 16:42:32 EDT";
-  public static final long BUILD_UNIX_TIME = 1774730552579L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-28 16:45:18 EDT";
+  public static final long BUILD_UNIX_TIME = 1774730718948L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
