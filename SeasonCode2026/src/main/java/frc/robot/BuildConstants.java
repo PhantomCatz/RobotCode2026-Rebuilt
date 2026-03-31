ae81d074492b1807c8790311178d734f89e33ec7
William W <149019293+soap-will-i-am@users.noreply.github.com>
2026-03-31 16:40:44 -0700

    probably works (confirmed 5 main paths: Solo outpost 2 cycle, outpost 2 cycle bump, decon depot 2 cycle bump fast, depot 2 cycle bump, depot 2 cycle bump full hopper)

--- SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 552;
-  public static final String GIT_SHA = "cef6f199e5573bdab29f7a7fa7a421ef06edb52c";
-  public static final String GIT_DATE = "2026-03-30 23:51:08 EDT";
+  public static final int GIT_REVISION = 561;
+  public static final String GIT_SHA = "4b0e8154c50cfa833957241eb56b92dd3abfe762";
+  public static final String GIT_DATE = "2026-03-31 18:31:02 EDT";
   public static final String GIT_BRANCH = "0.1-second-delay";
-  public static final String BUILD_DATE = "2026-03-31 00:24:29 EDT";
-  public static final long BUILD_UNIX_TIME = 1774931069301L;
+  public static final String BUILD_DATE = "2026-03-31 19:16:35 EDT";
+  public static final long BUILD_UNIX_TIME = 1774998995756L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
