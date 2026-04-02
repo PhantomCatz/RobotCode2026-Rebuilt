7ae365d430ac4bf9099428da695ee97b9f9f6b4e
William W <149019293+soap-will-i-am@users.noreply.github.com>
2026-04-01 19:48:17 -0700

    SoloOutpost and Depot2CycleBumpFullHopper confirmd

--- SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 559;
-  public static final String GIT_SHA = "74966b686c3a311aaba16e692283bc446a085b68";
-  public static final String GIT_DATE = "2026-03-31 17:31:57 EDT";
+  public static final int GIT_REVISION = 565;
+  public static final String GIT_SHA = "b80dc52b1461b5b7f705b82e984a6c406e711c72";
+  public static final String GIT_DATE = "2026-04-01 19:01:37 EDT";
   public static final String GIT_BRANCH = "0.1-second-delay";
-  public static final String BUILD_DATE = "2026-03-31 23:26:50 EDT";
-  public static final long BUILD_UNIX_TIME = 1775014010928L;
+  public static final String BUILD_DATE = "2026-04-01 22:47:56 EDT";
+  public static final long BUILD_UNIX_TIME = 1775098076651L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
