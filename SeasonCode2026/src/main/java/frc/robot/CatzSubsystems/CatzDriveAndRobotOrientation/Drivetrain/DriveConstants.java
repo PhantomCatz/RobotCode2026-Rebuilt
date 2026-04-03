ceb58d9a8b1980f27624ab65de4f4a4124df1c22
William <149019293+soap-will-i-am@users.noreply.github.com>
2026-04-03 07:35:16 -0700

    lower rotation p

--- SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzDriveAndRobotOrientation/Drivetrain/DriveConstants.java
+++ SeasonCode2026/src/main/java/frc/robot/CatzSubsystems/CatzDriveAndRobotOrientation/Drivetrain/DriveConstants.java
@@ -205,7 +205,7 @@ public class DriveConstants {
       new PIDController(10.0, 0.0, 0.3),
       new PIDController(10.0, 0.0, 0.3),
       new ProfiledPIDController(
-        17.0,
+        13.0,
         0.0,
         0.3,
         new TrapezoidProfile.Constraints(TRAJECTORY_CONFIG.maxAngularVelocity, TRAJECTORY_CONFIG.maxAngularAcceleration)
