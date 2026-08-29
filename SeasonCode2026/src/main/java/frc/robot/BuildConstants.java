85dc0b5bcfa6a5bd13cc0e29af7d874094be962a
Soap_will_i_am <w1wangwilliam@gmail.com>
2026-08-29 09:41:04 -0700

    Aux move

--- SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 429;
-  public static final String GIT_SHA = "731106ba88bd8af137bd7e42ebfc47ae035aa53a";
-  public static final String GIT_DATE = "2026-08-27 01:21:41 EDT";
+  public static final int GIT_REVISION = 430;
+  public static final String GIT_SHA = "002af11a63455f7e087c5a34d6f58f094a1f887c";
+  public static final String GIT_DATE = "2026-08-29 12:12:42 EDT";
   public static final String GIT_BRANCH = "syscore-shotblocker";
-  public static final String BUILD_DATE = "2026-08-29 11:56:54 EDT";
-  public static final long BUILD_UNIX_TIME = 1788019014725L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-08-29 12:38:58 EDT";
+  public static final long BUILD_UNIX_TIME = 1788021538384L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
