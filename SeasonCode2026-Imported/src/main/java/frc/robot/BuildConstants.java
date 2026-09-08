58fe4babbd66b13f979bb1300efba8b8f2faa77b
Nathan Kwok <sciencenatekwok@gmail.com>
2026-09-07 17:17:16 -0700

    update limelight vendordep

--- SeasonCode2026-Imported/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2026-Imported/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2026-Imported";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 449;
-  public static final String GIT_SHA = "c92bba2fe235b42b3098762c10d0a65be4e636d9";
-  public static final String GIT_DATE = "2026-09-04 00:24:54 EDT";
+  public static final int GIT_REVISION = 450;
+  public static final String GIT_SHA = "68eac5e4f5a16c96c2570cd41853de76d0df688b";
+  public static final String GIT_DATE = "2026-09-04 22:18:39 EDT";
   public static final String GIT_BRANCH = "syscore-test-no-choreo";
-  public static final String BUILD_DATE = "2026-09-04 22:18:02 EDT";
-  public static final long BUILD_UNIX_TIME = 1788574682991L;
+  public static final String BUILD_DATE = "2026-09-07 20:16:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1788826606235L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
