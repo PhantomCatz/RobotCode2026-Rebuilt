8f746cd6cb3d18a8a57628a3a8219ba6d39185e7
William <149019293+soap-will-i-am@users.noreply.github.com>
2026-09-12 12:57:26 -0700

    Revert "Merge branch 'syscore-test-no-choreo' of https://github.com/PhantomCatz/RobotCode2026-Rebuilt into syscore-test-no-choreo"
    This reverts commit c655c09c0a19f32008342a52b26f17e0778dd7da, reversing
    changes made to 5da624e65aed9b4a11c0ef137de4ec23a8f3e331.

--- SeasonCode2026-Imported/src/main/java/frc/robot/CatzAbstractions/io/GenericTalonFXIOReal.java
+++ SeasonCode2026-Imported/src/main/java/frc/robot/CatzAbstractions/io/GenericTalonFXIOReal.java
@@ -56,13 +56,13 @@ public abstract class GenericTalonFXIOReal<T extends GenericMotorIO.MotorIOInput
 	private final boolean[] connectedBuffer;
 
 	public GenericTalonFXIOReal(MotorIOTalonFXConfig config, boolean requiresFastUpdate) {
-		leaderTalon = new TalonFX(config.mainID, CANBus.systemcore(config.mainBus));
+		leaderTalon = new TalonFX(config.mainID,  CANBus.systemcore(config.mainBus));
 		setMainConfig(config.mainConfig);
 
 		if (config.followerIDs.length != 0) {
 			followerTalons = new TalonFX[config.followerIDs.length];
 			for (int i = 0; i < config.followerIDs.length; i++) {
-				followerTalons[i] = new TalonFX(config.followerIDs[i], new CANBus(config.followerBuses[i]));
+				followerTalons[i] = new TalonFX(config.followerIDs[i], CANBus.systemcore(config.followerBuses[i]));
 				followerTalons[i].setControl(new Follower(config.mainID, config.followerAlignmentValue[i]));
 			}
 			setFollowerConfig(followerConfig);
@@ -133,7 +133,6 @@ public abstract class GenericTalonFXIOReal<T extends GenericMotorIO.MotorIOInput
 	public void updateInputs(T inputs) {
 
 		inputs.isLeaderConnected = internalPositionRotations.getStatus().isOK();
-
 		if(followerTalons != null && followerTalons.length > 0) {
 			for(int i = 0; i < followerTalons.length; i++) {
 				connectedBuffer[i] = appliedVoltage.get(i+1).getStatus().isOK();
@@ -327,10 +326,10 @@ public abstract class GenericTalonFXIOReal<T extends GenericMotorIO.MotorIOInput
 
 	public static class MotorIOTalonFXConfig {
 		public int mainID = -1;
-		public int mainBus = -1;
+		public int mainBus = 0;
 		public TalonFXConfiguration mainConfig = new TalonFXConfiguration();
 		public int[] followerIDs = new int[0];
-		public String[] followerBuses = new String[0];
+		public int[] followerBuses = new int[0];
 		public TalonFXConfiguration followerConfig = new TalonFXConfiguration();
 		public MotorAlignmentValue[] followerAlignmentValue = new MotorAlignmentValue[0];
 	}
--- SeasonCode2026-Imported/src/main/java/frc/robot/CatzConstants.java
+++ SeasonCode2026-Imported/src/main/java/frc/robot/CatzConstants.java
@@ -16,11 +16,11 @@ public final class CatzConstants {
   //
   // --------------------------------------------------
   public static final RobotScenario robotScenario = RobotScenario.COMPETITION;
-  public static final RobotHardwareMode hardwareMode = RobotHardwareMode.SIM;
-  private static RobotID robotType = RobotID.SN_TEST;
+  public static final RobotHardwareMode hardwareMode = RobotHardwareMode.REAL;
+  private static RobotID robotType = RobotID.SN2;
   private static AlertPriority alertWarningPriority = AlertPriority.ONE;
   public static boolean disableHAL = false;
-  
+
   public static final double LOOP_TIME = 0.02;
 
   // public static AutoFactory autoFactory;
