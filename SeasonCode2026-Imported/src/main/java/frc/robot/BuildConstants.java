381cf4c319656bdc3d38b38a97fa025f6d214d99
Nathan Kwok <sciencenatekwok@gmail.com>
2026-09-25 09:22:54 -0700

    add simgui-ds.json to gitignore

--- SeasonCode2026-Imported/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2026-Imported/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2026-Imported";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 479;
-  public static final String GIT_SHA = "7f9667d8c7e2279f4a1a9d3f5e38c336b5716c02";
-  public static final String GIT_DATE = "2026-09-24 20:31:30 EDT";
+  public static final int GIT_REVISION = 480;
+  public static final String GIT_SHA = "8501c2a619621b7e777b81ea332409b17a56f1d0";
+  public static final String GIT_DATE = "2026-09-24 22:20:42 EDT";
   public static final String GIT_BRANCH = "syscore-test";
-  public static final String BUILD_DATE = "2026-09-24 22:09:28 EDT";
-  public static final long BUILD_UNIX_TIME = 1790302168874L;
+  public static final String BUILD_DATE = "2026-09-25 13:11:01 EDT";
+  public static final long BUILD_UNIX_TIME = 1790356261718L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
